b30594bfd4ef21886647daa883dffc2fd1ace7ac
James Prior <jamesgr.prior@gmail.com>
2024-05-12 19:07:54 +0100

    Stub python bindings

---
+++ python/src/lib.rs
@@ -0,0 +1,15 @@
+use iregexp::check;
+use pyo3::prelude::*;
+
+// TODO: docs
+#[pyfunction]
+#[pyo3(name = "check")]
+fn py_check(pattern: &str) -> bool {
+    check(pattern)
+}
+
+#[pymodule]
+fn py_iregexp(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_check, m)?)?;
+    Ok(())
+}
